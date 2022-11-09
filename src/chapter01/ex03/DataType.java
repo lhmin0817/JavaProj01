f5ddc515f06039e98eff036265b98489429456cc
Hwanmin <qwop987@naver.com>
2022-11-09 14:56:24 +0900

    22.11.09-2

---
+++ src/chapter01/ex03/DataType.java
@@ -0,0 +1,24 @@
+package chapter01.ex03;
+
+public class DataType {
+	public static void main(String[] args) {
+		// 변수를 생성 후 출력하기
+		// 변수를 선언하기 전에 어떤 데이터 형식을 저장할지 선언
+		int a; 		//변수 선언	int : 정수를 담는 정수
+		a = 10;
+		System.out.println(a);	//10
+		a = 20;
+		System.out.println(a);	//20
+		
+		//a = 10.5;				//실수는 담을 수 없다.
+		float b; 				//float : 실수를 저장할 수 있는 변수 
+		b=10;					
+		System.out.println(b);
+		double c;				//double : 실수를 저장할 수 있는 변수
+		c=10.5;					
+		System.out.println(c);
+				
+			
+		
+	}
+}
